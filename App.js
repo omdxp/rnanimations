6ed9cf487071581ad98ed52a2eeb4e8f0a2e11e9
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-12 14:43:31 +0100

    ❤ Moving ball.

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import {Animated, View} from 'react-native';
+import React, {useState} from 'react';
+import {Animated, TouchableOpacity, View, Text} from 'react-native';
 
 export default function App() {
+  const value = useState(new Animated.ValueXY({x: 0, y: 0}))[0];
+
+  const moveBall = () => {
+    Animated.timing(value, {
+      toValue: {x: 100, y: 100},
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
   return (
     <View>
-      <Animated.View>
+      <Animated.View style={value.getLayout()}>
         <View
           style={{
             width: 100,
             height: 100,
             borderRadius: 100 / 2,
             backgroundColor: 'blue',
-          }}></View>
+          }}
+        />
       </Animated.View>
+      <TouchableOpacity
+        onPress={moveBall}
+        style={{backgroundColor: 'red', width: 100}}>
+        <Text style={{fontSize: 20}}>Move ball</Text>
+      </TouchableOpacity>
     </View>
   );
 }
