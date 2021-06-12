2e8e4fc6e80faf7a115c571f0ecda7092d1c731f
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-06-12 17:50:00 +0100

    ✈ Pan responder with animations.

--- App.js
+++ App.js
@@ -1,42 +1,39 @@
 import React, {useState} from 'react';
-import {View, Animated, TouchableOpacity, Text} from 'react-native';
+import {Animated, View, Text, PanResponder} from 'react-native';
 
 export default function App() {
-  const value = useState(new Animated.Value(0))[0];
+  const pan = useState(new Animated.ValueXY())[0];
 
-  const fadeIn = () => {
-    Animated.timing(value, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(value, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
-  };
+  const panResponder = useState(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    }),
+  )[0];
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1}}>
       <Animated.View
-        style={{
-          width: 100,
-          height: 100,
-          borderRadius: 100 / 2,
-          backgroundColor: 'red',
-          opacity: value,
-        }}
+        style={[
+          {
+            width: 100,
+            height: 100,
+            borderRadius: 100 / 2,
+            backgroundColor: 'red',
+          },
+          pan.getLayout(),
+        ]}
+        {...panResponder.panHandlers}
       />
-      <TouchableOpacity onPress={fadeIn}>
-        <Text>Fade in</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={fadeOut}>
-        <Text>Fade out</Text>
-      </TouchableOpacity>
     </View>
   );
 }
